d0fb034bb59c1688851d004711e24aa8c45a3e64
Rick Brown <ricky.brown.00@gmail.com>
2022-02-01 21:54:56 +0000

    stopped at 'Listing the invoices'

---
+++ src/routes/expenses.jsx
@@ -0,0 +1,7 @@
+export default function Expenses() {
+	return (
+		<main style={{ padding: '1rem 0', height: '90vh' }}>
+			<h2>Expenses</h2>
+		</main>
+	)
+}
---
+++ src/routes/invoices.jsx
@@ -0,0 +1,7 @@
+export default function Invoices() {
+	return (
+		<main style={{ padding: '1rem 0', height: '90vh' }}>
+			<h2>Invoices</h2>
+		</main>
+	)
+}
